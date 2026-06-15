b1d36d8b3156436a983131a324392fc5602fb2f8
User-MiMa <mlsmasson@outlook.com>
2026-06-15 12:12:29 -0600

    feat: add system message in server.js
    refactor: initialize array directly in declaration

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 // Importar SDK de open route; uso de funciones avanzadas
-// Importar dotenv apra usar variables de entorno de .env
+// Importar dotenv para usar variables de entorno de .env
 import { OpenRouter } from '@openrouter/sdk';
 import 'dotenv/config';
 
@@ -12,17 +12,30 @@ const client = new OpenRouter({
 // Mensaje/Prompt de usuario 
 const userPrompt = "What is the meaning of life?";
 
+// Reglas para modelo
+const systemPrompt = `You are an efficient and smart assistant. Your goal is
+                        to provide answers that are as short as possible and
+                        simple to understand to any person. 
+                        If the user prompt lacks enough context to achieve the main goal,
+                        ask questions to provide a more accurate answer`;
+
 // Envolver mensaje/prompt para API openRoute
-const userMessages = { 
-    role: "user", 
-    content: userPrompt 
-}
+const userMessages = [
+    {
+        role:"system",
+        content: systemPrompt
+    },
+    { 
+        role: "user", 
+        content: userPrompt 
+    }
+];
 
 //Llamar API con mensaje/prompt y modelo deseado
 const response = await client.chat.send({
   chatRequest: {
     model: process.env.AI_MODEL,
-    messages: [userMessages]
+    messages: userMessages
   }
 });
 
