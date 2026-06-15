fbaf1d226f275e3d04c268b482563e4f11529c5e
User-MiMa <mlsmasson@outlook.com>
2026-06-14 21:36:12 -0600

    refactor: improve code clarity in server.js

--- server/server.js
+++ server/server.js
@@ -1,19 +1,31 @@
+// Importar SDK de open route; uso de funciones avanzadas
+// Importar dotenv apra usar variables de entorno de .env
 import { OpenRouter } from '@openrouter/sdk';
 import 'dotenv/config';
 
+// Inicializar cliente SDK OpenRouter con credenciales y URL base
 const client = new OpenRouter({
   apiKey: process.env.AI_KEY,
   baseURL: process.env.AI_URL,
 });
 
-const completion = await client.chat.send({
+// Mensaje/Prompt de usuario 
+const userPrompt = "What is the meaning of life?";
+
+// Envolver mensaje/prompt para API openRoute
+const userMessages = { 
+    role: "user", 
+    content: userPrompt 
+}
+
+//Llamar API con mensaje/prompt y modelo deseado
+const response = await client.chat.send({
   chatRequest: {
     model: process.env.AI_MODEL,
-    messages: [
-      { role: "user", content: "What is the meaning of life?" }
-    ]
+    messages: [userMessages]
   }
 });
 
-console.log(completion.choices[0].message.content);
+// Mostrar respuesta de modelo
+console.log(response.choices[0].message.content);
 
