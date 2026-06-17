d19c8ede6984f13e99c012f459555dd849cf38d2
User-MiMa <mlsmasson@outlook.com>
2026-06-16 22:17:54 -0600

    feat: add code to initialize server in server.js
    feat: add backend logic in server.js to process user prompt
    
    feat: add frontend logic in script.js to send and receive model answer
    
    style: improvement in css file style

--- public/script.js
+++ public/script.js
@@ -1,13 +1,29 @@
 const btn = document.getElementById('button');
 const answerWindow = document.querySelector('.answer');
-const prompt = document.getElementById('promptSpace');
 
 btn.addEventListener('click', showAnswer);
 
-function showAnswer() {
+async function showAnswer() {
+
+    const prompt = document.getElementById('promptSpace').value;
+
     answerWindow.style.width='50vw';
     answerWindow.style.height='50vw';
     answerWindow.style.border='3px solid';
 
-    answerWindow.textContent=prompt.value;
+    // Hacer POST a API con prompt de usuario del UI y recibir respuesta
+    const res = await fetch('/chat', 
+        {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({prompt})
+        }
+    )
+
+    // Leer y parsear a JSON
+    const data = await res.json();
+
+    // Mostrar respuesta del modelo en UI
+    answerWindow.textContent = data.reply;
+
 }
--- server/server.js
+++ server/server.js
@@ -1,16 +1,22 @@
 // Importar SDK de open route; uso de funciones avanzadas
 // Importar dotenv para usar variables de entorno de .env
+// Importar express para inicializar servidor
 import { OpenRouter } from '@openrouter/sdk';
 import 'dotenv/config';
+import express from 'express';
+
+//Instanciar servidor
+// Middleware para convertir body de peticiones entrantes de JSON a JS Object
+// Servir archivos estáticos
+const app = express();
+app.use(express.json());
+app.use(express.static('public'));
 
 // Inicializar cliente SDK OpenRouter con credenciales y URL base
 const client = new OpenRouter({
   apiKey: process.env.AI_KEY,
   baseURL: process.env.AI_URL,
 });
-
-// Mensaje/Prompt de usuario 
-const userPrompt = "What is the meaning of life?";
 
 // Reglas para modelo
 const systemPrompt = `You are an efficient and smart assistant. Your goal is
@@ -19,26 +25,33 @@ const systemPrompt = `You are an efficient and smart assistant. Your goal is
                         If the user prompt lacks enough context to achieve the main goal,
                         ask questions to provide a more accurate answer`;
 
+
 // Envolver mensaje/prompt para API openRoute
 const userMessages = [
     {
         role:"system",
         content: systemPrompt
     },
-    { 
-        role: "user", 
-        content: userPrompt 
-    }
 ];
 
-//Llamar API con mensaje/prompt y modelo deseado
-const response = await client.chat.send({
-  chatRequest: {
-    model: process.env.AI_MODEL,
-    messages: userMessages
-  }
+// Crear endpoint HTTP POST para
+app.post('/chat', async function(req,res){
+
+    //Agregar prompt de usuario a arreglo
+    userMessages.push({role: 'user', content: `${req.body.prompt}`})
+
+    //Llamar API con mensaje/prompt y modelo deseado
+    const response = await client.chat.send({
+        chatRequest: {
+            model: process.env.AI_MODEL,
+            messages: userMessages
+        }
+    });
+
+    // Convertir en json la respuesta obtenida del modelo para devolver al front
+    res.json({reply: response.choices[0].message.content});
+
 });
 
-// Mostrar respuesta de modelo
-console.log(response.choices[0].message.content);
-
+//Inicar servidor en puerto 3000
+app.listen(3000);
